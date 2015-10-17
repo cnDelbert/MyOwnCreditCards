71022e9eedaf5f4a665129408c434a00c302755c
Delbert <code@delbert.me>
2015-10-17 22:51:53 +0800

    Init

---
+++ config/config.inc.sample.php
@@ -0,0 +1,4 @@
+<?php
+define("DB_HOST", "host_address");
+define("DB_PORT", "host_port");
+define("DB_USER", "db_user");
--- index.php
+++ index.php
@@ -5,3 +5,5 @@
  * Date: 15/10/17
  * Time: 下午3:56
  */
+?>
+<h1>Hello, world!</h1>
