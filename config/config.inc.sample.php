38f49b191ffe8687d6687dd586a631385268a936
Delbert <code@delbert.me>
2015-10-19 23:12:38 +0800

    structor

--- config/config.inc.sample.php
+++ config/config.inc.sample.php
@@ -2,3 +2,6 @@
 define("DB_HOST", "host_address");
 define("DB_PORT", "host_port");
 define("DB_USER", "db_user");
+define("DB_PSWD", "db_pswd");
+define("DB_NAME", "db_name");
+define("SALT","");
---
+++ inc/function.inc.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Delbert
+ * Date: 15/10/19
+ * Time: 下午11:11
+ */
