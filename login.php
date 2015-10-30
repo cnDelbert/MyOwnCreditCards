04d2966ba209db1cb7e45aea4730467abd06bdba
Delbert <code@delbert.me>
2015-10-30 23:42:40 +0800

    Test mysqli

--- login.php
+++ login.php
@@ -6,9 +6,12 @@
  * Time: 下午11:16
  */
 require_once("./inc/header.inc.php");
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PSWD, DB_NAME, DB_PORT);
+var_dump($conn);
+var_dump(mysqli_query($conn, "SELECT * FROM MyCCs_User"));
 ?>
 
 
 <?php
-var_dump($_SERVER);
+//var_dump($_SERVER);
 require_once("./inc/footer.inc.php");
